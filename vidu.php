9e5b7f57c5785e0de16ed859a13d0a9ccda6943b
Nguyen Danh Ha <danhha221020@gmail.com>
2021-04-28 22:59:34 +0700

    Them h2

--- vidu.php
+++ vidu.php
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 <h1>Tạo File vidu.php . Thêm từ visual stdio code</h1>
-=======
-<h1>Tạo File vidu.php . Thêm từ Website !</h1>
->>>>>>> e134fc1d7ba557abe4021f1ad3be7daf9af86b44
+<h2>Chào Các Bạn Nhé !</h2>
