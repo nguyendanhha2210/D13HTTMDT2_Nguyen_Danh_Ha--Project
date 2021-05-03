160e8bd9eaa46fa0d1e874a8cb129de498b95409
Nguyen Danh Ha <danhha221020@gmail.com>
2021-05-04 02:21:34 +0700

    Them chao 3

--- abc.php
+++ abc.php
@@ -1,6 +1,3 @@
 <h1>Chào nhé 1 !</h1>
-<<<<<<< HEAD
 <h1>Chào nhé 2 ! Thên từ visual</h1>
-=======
-<h1>Chào nhé 2 Thêm từ Web !</h1>
->>>>>>> e2b7c4765f49fb3084647e081c79fa606ad76633
+<h1>Chào nhé 3!</h1>
